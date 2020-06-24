cb901d1475b66d16b246922e885d857fc3816f84
daniel <danielmtgiraldo@gmail.com>
2020-06-24 11:20:37 -0500

    Updates

---
+++ precache-manifest.cb1a0de7f64c6c741d7cb872bc5e844f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba8262d435fe871a0e23141d2f093b1",
+    "url": "/-weather-grafics-app-react.js/index.html"
+  },
+  {
+    "revision": "2f8c3dae86ce710e9855",
+    "url": "/-weather-grafics-app-react.js/static/css/main.a3b53faf.chunk.css"
+  },
+  {
+    "revision": "af7d9e01b1edf2fb7a9b",
+    "url": "/-weather-grafics-app-react.js/static/js/2.07e02518.chunk.js"
+  },
+  {
+    "revision": "8d26b2e3700774aaa3e681d097f59854",
+    "url": "/-weather-grafics-app-react.js/static/js/2.07e02518.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f8c3dae86ce710e9855",
+    "url": "/-weather-grafics-app-react.js/static/js/main.8d27cb51.chunk.js"
+  },
+  {
+    "revision": "0cb84b2f19444dd569b0",
+    "url": "/-weather-grafics-app-react.js/static/js/runtime-main.34825a3b.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/-weather-grafics-app-react.js/precache-manifest.cb1a0de7f64c6c741d7cb872bc5e844f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/-weather-grafics-app-react.js/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
